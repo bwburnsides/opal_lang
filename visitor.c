d8be1a5172a1c441b713b531baad7b132b0590ca
brady <brady@burnsides.us>
2022-11-13 17:19:25 -0600

    simplified visitor implementation

--- visitor.c
+++ visitor.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define EXPR_ACCEPT(expr, visitor_kind) (expr)->base.accept((Expression*) expr, (visitor_kind))
+
 typedef struct ExpressionT Expression;
 typedef struct ExpressionVisitorT ExpressionVisitor;
 
@@ -9,7 +11,23 @@ typedef enum ExpressionKindT {
     BinaryExpressionKind,
 } ExpressionKind;
 
-typedef void (*ExpressionVisitorAcceptFunction)(Expression*, ExpressionVisitor*);
+typedef enum ExpressionVisitorKindT {
+    PrintExpressionVisitor,
+    //
+    ExpressionVisitorCount
+} ExpressionVisitorKind;
+
+ExpressionVisitor* EXPRESSION_VISITORS[ExpressionVisitorCount];
+
+typedef void (*ExpressionVisitorFunction)(ExpressionVisitor*, Expression*);
+
+typedef struct ExpressionVisitorT {
+    ExpressionVisitorKind kind;
+    ExpressionVisitorFunction unary_expression;
+    ExpressionVisitorFunction binary_expression;
+} ExpressionVisitor;
+
+typedef void (*ExpressionVisitorAcceptFunction)(Expression*, ExpressionVisitorKind);
 
 typedef struct ExpressionT {
     ExpressionKind kind;
@@ -22,6 +40,11 @@ typedef struct UnaryExpressionT {
     char* right;
 } UnaryExpression;
 
+void UnaryExpressionAccept(Expression* expr, ExpressionVisitorKind visitor_kind) {
+    ExpressionVisitor* visitor = EXPRESSION_VISITORS[visitor_kind];
+    visitor->unary_expression(visitor, expr);
+}
+
 typedef struct BinaryExpressionT {
     Expression base;
     char* left;
@@ -29,15 +52,9 @@ typedef struct BinaryExpressionT {
     char* right;
 } BinaryExpression;
 
-void ExpressionVisitorAccept(Expression* expr, ExpressionVisitor* visitor) {
-    switch (expr->kind) {
-        case UnaryExpressionKind:
-            break;
-        case BinaryExpressionKind:
-            break;
-        default:
-            return;
-    }
+void BinaryExpressionAccept(Expression* expr, ExpressionVisitorKind visitor_kind) {
+    ExpressionVisitor* visitor = EXPRESSION_VISITORS[visitor_kind];
+    visitor->binary_expression(visitor, expr);
 }
 
 void* NewExpression(ExpressionKind kind) {
@@ -46,9 +63,15 @@ void* NewExpression(ExpressionKind kind) {
     switch (kind) {
         case UnaryExpressionKind:
             expr = malloc(sizeof(UnaryExpression));
+            if (expr != NULL) {
+                ((Expression*) expr)->accept = &UnaryExpressionAccept;
+            }
             break;
         case BinaryExpressionKind:
             expr = malloc(sizeof(BinaryExpression));
+            if (expr != NULL) {
+                ((Expression*) expr)->accept = &BinaryExpressionAccept;
+            }
             break;
         default:
             return NULL;
@@ -56,38 +79,12 @@ void* NewExpression(ExpressionKind kind) {
 
     if (expr != NULL) {
         ((Expression*) expr)->kind = kind;
-        ((Expression*) expr)->accept = &ExpressionVisitorAccept;
     }
 
     return expr;
 }
 
-typedef enum ExpressionVisitorKindT {
-    PrintExpressionVisitor,
-    ExpressionVisitorCount
-} ExpressionVisitorKind;
-
-typedef void (*ExpressionVisitorFunction)(ExpressionVisitor*, Expression*);
-
-typedef struct ExpressionVisitorT {
-    ExpressionVisitorKind kind;
-    void (*visit)(ExpressionVisitor* visitor, Expression* expr);
-    ExpressionVisitorFunction unary_expression;
-    ExpressionVisitorFunction binary_expression;
-} ExpressionVisitor;
-
 ExpressionVisitor* EXPRESSION_VISITORS[ExpressionVisitorCount];
-
-void ExpressionVisit(ExpressionVisitor* visitor, Expression* expr) {
-    switch (expr->kind) {
-        case UnaryExpressionKind:
-            return visitor->unary_expression(visitor, expr);
-        case BinaryExpressionKind:
-            return visitor->binary_expression(visitor, expr);
-        default:
-            return;
-    }
-}
 
 void PrintUnaryExpression(ExpressionVisitor* visitor, Expression* expr) {
     UnaryExpression* uny = (UnaryExpression*) expr;
@@ -103,14 +100,6 @@ void PrintBinaryExpression(ExpressionVisitor* visitor, Expression* expr) {
         "BinaryExpression(\n\tleft=\"%s\"\n\top=\"%s\"\n\tright=\"%s\"\n)\n",
         bin->left, bin->op, bin->right
     );
-}
-
-void Visit(ExpressionVisitorKind kind, Expression* expr) {
-    if (kind == ExpressionVisitorCount) {
-        return;
-    }
-
-    EXPRESSION_VISITORS[kind]->visit(EXPRESSION_VISITORS[kind], expr);
 }
 
 void DefaultVisitorHandler(ExpressionVisitor* visitor, Expression* expr) {
@@ -129,7 +118,6 @@ void InitializeExpressionVisitors() {
             exit(-1);
         }
         EXPRESSION_VISITORS[i]->kind = i;
-        EXPRESSION_VISITORS[i]->visit = &ExpressionVisit;
         EXPRESSION_VISITORS[i]->unary_expression = &DefaultVisitorHandler;
         EXPRESSION_VISITORS[i]->binary_expression = &DefaultVisitorHandler;
     }
@@ -142,18 +130,22 @@ int main(int argc, char** argv) {
     InitializeExpressionVisitors();
 
     UnaryExpression* uny = NewExpression(UnaryExpressionKind);
-    if (uny == NULL) { return -1; }
+    if (uny == NULL) {
+        return -1;
+    }
     uny->right = "b";
     uny->op = "!";
 
     BinaryExpression* bin = NewExpression(BinaryExpressionKind);
-    if (bin == NULL) { return -1; }
+    if (bin == NULL) {
+        return -1;
+    }
     bin->left = "a";
     bin->op = "+";
     bin->right = "c";
 
-    Visit(PrintExpressionVisitor, (Expression*) bin);
-    Visit(PrintExpressionVisitor, (Expression*) uny);
+    EXPR_ACCEPT(uny, PrintExpressionVisitor);
+    EXPR_ACCEPT(bin, PrintExpressionVisitor);
 
     return 0;
 }
