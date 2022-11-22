a620f07efbd68d88e6d482131de5fdead88ee115
brady <brady@burnsides.us>
2022-11-21 20:42:12 -0600

    initial parser code

--- src/parser.c
+++ src/parser.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdarg.h>
 #include "parser.h"
 #include "expression.h"
 
@@ -20,37 +21,176 @@ Expr* parser_expression(Parser* self) {
     return parser_equality(self);
 }
 
-Expr* parser_comparison(Parser* self) {
-    return NULL;
-}
-
 Expr* parser_equality(Parser* self) {
     Expr* expr = parser_comparison(self);
     Token* operator;
-    BinaryExpr* right;
+    Expr* right;
 
-    while (parser_match(self, Token_ExclamationEqual, Token_Equal)) {
+    while (
+        parser_match(self, 2,
+            Token_ExclamationEqual,
+            Token_Equal
+        )
+    ) {
         operator = parser_previous(self);
-        right = (BinaryExpr*) parser_comparison(self);
-        expr = (Expr*) binaryexpr_init(expr, operator, (Expr*)right);
+        right = parser_comparison(self);
+        expr = (Expr*) binaryexpr_init(expr, operator, right);
     }
 }
 
-bool parser_match(Parser* self, ...) {
-    return false;
+Expr* parser_comparison(Parser* self) {
+    Expr* expr = parser_term(self);
+    Token* operator;
+    Expr* right;
+
+    while (
+        parser_match(self, 4,
+            Token_GreaterThan,
+            Token_GreaterThanEqual,
+            Token_LessThan,
+            Token_LessThanEqual
+        )
+    ) {
+        operator = parser_previous(self);
+        right = parser_term(self);
+        expr = (Expr*) binaryexpr_init(expr, operator, right);
+    }
+
+    return expr;
 }
+
+Expr* parser_term(Parser* self) {
+    Expr* expr = parser_factor(self);
+    Token* operator;
+    Expr* right;
+
+    while (
+        parser_match(self, 2,
+            Token_Plus,
+            Token_Minus
+        )
+    ) {
+        operator = parser_previous(self);
+        right = parser_factor(self);
+        expr = (Expr*) binaryexpr_init(expr, operator, right);
+    }
+
+    return expr;
+}
+
+Expr* parser_factor(Parser* self) {
+    Expr* expr = parser_unary(self);
+    Token* operator;
+    Expr* right;
+
+    while (
+        parser_match(self, 2,
+            Token_FSlash,
+            Token_Asterisk
+        )
+    ) {
+        operator = parser_previous(self);
+        right = parser_unary(self);
+        expr = (Expr*) binaryexpr_init(expr, operator, right);
+    }
+
+    return expr;
+}
+
+Expr* parser_unary(Parser* self) {
+    Token* operator;
+    Expr* right;
+
+    if (
+        parser_match(self, 2,
+            Token_Exclamation,
+            Token_Minus
+        )
+    ) {
+        operator = parser_previous(self);
+        right = parser_unary(self);
+        return (Expr*) unaryexpr_init(operator, right);
+    }
+
+    return parser_primary(self);
+}
+
+Expr* parser_primary(Parser* self) {
+    Expr* expr;
+
+    if (
+        parser_match(self, 3,
+            Token_BinIntegerLiteral,
+            Token_HexIntegerLiteral,
+            Token_DecIntegerLiteral
+        )
+    ) {
+        return (Expr*) literalexpr_init(parser_previous(self));
+    }
+
+    if (parser_match(self, 1, Token_LParen)) {
+        expr = parser_expression(self);
+        parser_consume(self, Token_RParen, "Expect ')' after expression.");
+        return (Expr*) groupingexpr_init(expr);
+    }
+}
+
+bool parser_match(Parser* self, size_t count, ...) {
+    va_list args;
+    bool matched = false;
+    TokenKind kind;
+
+    va_start(args, count);
+    for (int i = 0; i < count; i++) {
+        kind = va_arg(args, TokenKind);
+
+        if (parser_check(self, kind)) {
+            parser_advance(self);
+            matched = true;
+            break;
+        }
+    }
+
+    va_end(args);
+    return matched;
+}
+
 bool parser_check(Parser* self, TokenKind kind) {
-    false;
+    if (parser_at_end(self)) {
+        return false;
+    }
+    return parser_peek(self)->kind == kind;
 }
+
 Token* parser_advance(Parser* self) {
-    return NULL;
+    if (!parser_at_end(self)) {
+        self->current++;
+    }
+    return parser_previous(self);
 }
+
 bool parser_at_end(Parser* self) {
-    return false;
+    return parser_peek(self)->kind == Token_EOF;
 }
+
 Token* parser_peek(Parser* self) {
-    return NULL;
+    return self->tokens[self->current];
 }
+
 Token* parser_previous(Parser* self) {
-    return NULL;
+    return self->tokens[self->current - 1];
+}
+
+ParseResult parser_consume(Parser* self, TokenKind kind, char* msg) {
+    ParseResult result;
+    result.kind = ParseResult_Error;
+    result.value.error = ParseError_UnexpectedToken;
+
+    if (parser_check(self, kind)) {
+        result.kind = ParseResult_Match;
+        result.value.match = parser_advance(self);
+        return result;
+    }
+
+    return result;
 }
--- src/parser.h
+++ src/parser.h
@@ -14,18 +14,41 @@ typedef struct Parser_t {
     int current;
 } Parser;
 
+typedef enum ParseError_t {
+    ParseError_UnexpectedToken,
+    ParseError_UnexpectedEOF,
+} ParseError;
+
+typedef struct ParseResult_t {
+    enum {
+        ParseResult_Match,
+        ParseResult_NoMatch,
+        ParseResult_Error
+    } kind;
+
+    union {
+        ParseError error;
+        Token* match;
+    } value;
+} ParseResult;
+
 Parser* parser_init(Token** tokens);
 void parser_free(Parser* self);
 
 Expr* parser_expression(Parser* self);
-Expr* parser_comparison(Parser* self);
-Expr* parser_comparison(Parser* self);
 Expr* parser_equality(Parser* self);
+Expr* parser_comparison(Parser* self);
+Expr* parser_term(Parser* self);
+Expr* parser_factor(Parser* self);
+Expr* parser_unary(Parser* self);
+Expr* parser_primary(Parser* self);
 
-bool parser_match(Parser* self, ...);
+bool parser_match(Parser* self, size_t count, ...);
 bool parser_check(Parser* self, TokenKind kind);
 Token* parser_advance(Parser* self);
 bool parser_at_end(Parser* self);
 Token* parser_peek(Parser* self);
 Token* parser_previous(Parser* self);
+ParseResult parser_consume(Parser* self, TokenKind kind, char* msg);
+
 #endif
